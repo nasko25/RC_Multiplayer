ad8dc3ee469d545d8d730684c98262119b3b4c52
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-12-02 14:30:55 +0200

    Second commit

--- app.js
+++ app.js
@@ -13,10 +13,16 @@ app.use("/client", express.static(__dirname + "/client"));
 serv.listen(3000);
 console.log("Server started; listening on port 3000");
 
+
+var SOCKET_LIST = {};
 var io = require("socket.io")(serv, {}); // loads the file ("socket.io") and initilizes it; it returns an io object that 
 // has all the functionalities of the socket.io library
 io.sockets.on("connection", function(socket) { // the function will be called, whenever there is a connection to the server.
   console.log("Socket connection");
+  socket.id = Math.random(); // this assigns a unique id to the newly created socket. 
+  socket.x = 0;
+  socket.y = 0;   // assign x and y parameters to the socket. 
+  SOCKET_LIST[socket.id] = socket; // add the socket to the list of sockets. 
   
   socket.on("happy", function(data){    // this is listening for an emitted "happy" message from the client, and executes the function, if there is one.
     // data ^ is the recieved object that the client sent. (reason, in this case, is contained in the data object) 
@@ -29,6 +35,19 @@ io.sockets.on("connection", function(socket) { // the function will be called, w
   });
   
 });
+
+setInterval(function() { // this is a loop; the function will be called every frame at will run at 25 fps (it will be called every 40 milliseconds)
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+    socket.emit("newPosition", {
+      x:socket.x,
+      y:socket.y
+    });
+  }
+  
+}, 1000/25);
 
 /*
 in js:
