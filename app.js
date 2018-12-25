75870bd56b461fdd7312fcbedb4d4f1afc44e126
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2018-12-25 15:29:30 +0200

    Sixth commit

--- app.js
+++ app.js
@@ -15,15 +15,27 @@ console.log("Server started; listening on port 3000");
 
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){ 
+  var self = { // this is a constructor
+    x:250,
+    y:250,
+    id:id,
+    number: "" + Math.floor(10 * Math.random()) // every player has a random int associated with it. 
+  }
+  return self;
+};
+
 var io = require("socket.io")(serv, {}); // loads the file ("socket.io") and initilizes it; it returns an io object that 
 // has all the functionalities of the socket.io library
 io.sockets.on("connection", function(socket) { // the function will be called, whenever there is a connection to the server.
   console.log("Socket connection");
-  socket.id = Math.random(); // this assigns a unique id to the newly created socket. 
-  socket.x = 0;
-  socket.y = 0;   // assign x and y parameters to the socket. 
-  socket.number = "" + Math.floor(10 * Math.random()); // every socket will have a random int associated with it. 
+  socket.id = Math.random(); // this assigns a unique id to the newly created socket.  
   SOCKET_LIST[socket.id] = socket; // add the socket to the list of sockets. 
+  
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
   
   socket.on("happy", function(data){    // this is listening for an emitted "happy" message from the client, and executes the function, if there is one.
     // data ^ is the recieved object that the client sent. (reason, in this case, is contained in the data object) 
@@ -39,21 +51,22 @@ io.sockets.on("connection", function(socket) { // the function will be called, w
   // when a player disconnects, delete him from the socket list
   socket.on("disconnect", function() { // the disconnect event is done automatically
     delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
   });
   
 });
 
 setInterval(function() { // this is a loop; the function will be called every frame and will run at 25 fps (it will be called every 40 milliseconds)
   var pack = []; // every frame a package called pack is created; it will be send to every player connected & will conatin info about every connected player.
-  for (var i in SOCKET_LIST) {
+  for (var i in PLAYER_LIST) {
     // console.log(Object.keys(SOCKET_LIST).length + " " + i) // this logs the length of the array SOCKET_LIST (it is an object, so we need the Object.keys...) and i.
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+    var player = PLAYER_LIST[i];
+    player.x++;
+    player.y++;
     pack.push({
-      x:socket.x,
-      y:socket.y,
-      number:socket.number
+      x:player.x,
+      y:player.y,
+      number:player.number
     });
   }
   for (var j in SOCKET_LIST) { 
